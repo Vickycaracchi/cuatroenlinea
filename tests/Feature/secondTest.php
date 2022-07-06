8847445e8f3caea66664723761e01b67ef87920c
Vickycaracchi <vickypau1d@gmail.com>
2022-07-05 22:55:31 -0300

    Segundo test funcionando

--- tests/Feature/secondTest.php
+++ tests/Feature/secondTest.php
@@ -13,10 +13,30 @@ class secondTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_comprueba_mitad_vacia()
     {
-        $response = $this->get('/');
+        $jugar = file_get_contents('https://4enlinea.ddev.site/jugar/111222333444555666777');
 
-        $response->assertStatus(200);
+        $this->assertTrue((substr_count($jugar,'bg-gray-200')) == 21); //comprueba si la mitad de recuadros estan vacios
+        
+        
+    }
+
+    public function test_comprueba_mitad_con_rojos()
+    {
+        $jugar = file_get_contents('https://4enlinea.ddev.site/jugar/111222333444555666777');//comprueba la cantidad de recuadros rojos
+
+        $this->assertTrue(substr_count($jugar,'bg-red-500') == 11);
+    }
+
+
+    public function test_comprueba_mitad_con_celestes()
+    {
+        $jugar = file_get_contents('https://4enlinea.ddev.site/jugar/111222333444555666777');//comprueba la cantidad de recuadros celestes
+
+        $this->assertTrue(substr_count($jugar,'bg-sky-500') == 17);
     }
 }
+
+
+
