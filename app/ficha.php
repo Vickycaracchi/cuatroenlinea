dc3de8288d0be7a1ae18fb70af0466ee366e6d80
Vickycaracchi <vickypau1d@gmail.com>
2022-08-22 21:13:45 -0300

    clase ficha

---
+++ app/ficha.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+
+interface General_Fichas{
+
+    public function guardar_color(); 
+}
+
+
+class Ficha implements General_Fichas
+{
+    protected string $color_ficha;
+    
+
+    public function __construct($color){
+		$this->color_ficha = $color;
+	}
+
+    public function guardar_color(){
+        return $this->color_ficha;
+    }
+
+}
